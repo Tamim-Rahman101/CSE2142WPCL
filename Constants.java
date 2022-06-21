1332619650d89b6822aa0c6e838013dea4661cf9
Tamim Rahman <tamim101rahman@gmail.com>
2022-06-22 00:07:25 +0600

    Replaced string literals with constants

---
+++ Constants.java
@@ -0,0 +1,22 @@
+public class Constants {
+    public static final String ARG_A = "a";
+    public static final String ARG_C = "c";
+    public static final String ARG_R = "r";
+    public static final char ARG_ADD = '+';
+    public static final char ARG_FIND = '?';
+
+    public static final String STUDENT_FILE_NAME = "students.txt";
+    public static final String DATE_FORMAT_SYSTEM = "dd/mm/yyyy-hh:mm:ss a";
+
+    public static final String LOADING_MESSAGE = "Loading data ...";
+    public static final String LOADED_MESSAGE = "Data Loaded.";
+    public static final String LAST_UPDATED_MESSAGE = "\nList last updated on ";
+    public static final String COUNTED_WORD_MESSAGE = " word(s) found";
+    public static final String FOUND_MESSAGE = "We found it!";
+    public static final String NOT_FOUND_MESSAGE = "Student not found!";
+    public static final String WRONG_INPUT_MESSAGE = "Please enter any of these : a | c | r | +word | ?word";
+
+    public static final String SPLIT = ", ";
+    public static final char SPACE = ' ';
+
+}
--- StudentList.java
+++ StudentList.java
@@ -8,7 +8,7 @@ public class StudentList {
 		try {
 			BufferedReader studentFileReader = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt")));
+							new FileInputStream(Constants.STUDENT_FILE_NAME)));
 			 studentNames = studentFileReader.readLine();
 		} catch (Exception e){
 
@@ -18,13 +18,13 @@ public class StudentList {
 
 	public static void fileWriter(String studentNames, String studentToAdd) {
 		try {
-			Date d = new Date();
-			String df = "dd/mm/yyyy-hh:mm:ss a";
+			Date date = new Date();
+			String df = Constants.DATE_FORMAT_SYSTEM;
 			DateFormat dateFormat = new SimpleDateFormat(df);
-			String formattedDate= dateFormat.format(d);
+			String formattedDate= dateFormat.format(date);
 			BufferedWriter studentFileWriter = new BufferedWriter(
-					new FileWriter("students.txt", false));
-			studentFileWriter.write(studentNames + ", " + studentToAdd + "\nList last updated on " + formattedDate);
+					new FileWriter(Constants.STUDENT_FILE_NAME, false));
+			studentFileWriter.write(studentNames + Constants.SPLIT + studentToAdd + Constants.LAST_UPDATED_MESSAGE + formattedDate);
 			studentFileWriter.close();
 		}catch (Exception e){
 
@@ -34,77 +34,75 @@ public class StudentList {
 	public static void main(String[] args) {
 
 //		Check arguments
-		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");
+		if(args[0].equals(Constants.ARG_A)) {
+			System.out.println(Constants.LOADING_MESSAGE);
 			String studentNames = fileReadder();
-			System.out.println(studentNames);
-
-			String studentNameArray[] = studentNames.split(", ");
+			String studentNameArray[] = studentNames.split(Constants.SPLIT);
 			for(String student : studentNameArray) {
 				System.out.println(student);
 			}
-			System.out.println("Data Loaded.");
+			System.out.println(Constants.LOADED_MESSAGE);
 		}
 
-		else if(args[0].equals("r")) {
-			System.out.println("Loading data ...");
+		else if(args[0].equals(Constants.ARG_R)) {
+			System.out.println(Constants.LOADING_MESSAGE);
 			String studentNames = fileReadder();
 			char studentNameCharArray[] = studentNames.toCharArray();
 			int studentNumber = 0;
 			for(char c : studentNameCharArray) {
-				if(c ==' ') {
+				if(c == Constants.SPACE) {
 					studentNumber++;
 				}
 			}
-			String studentNameArray[] = studentNames.split(", ");
+			String studentNameArray[] = studentNames.split(Constants.SPLIT);
 			Random rand = new Random();
 			int randomStudentIndex = rand.nextInt(studentNumber + 1);
 			System.out.println(studentNameArray[randomStudentIndex]);
-			System.out.println("Data Loaded.");			
+			System.out.println(Constants.LOADED_MESSAGE);
 		}
 
-		else if(args[0].equals("c")) {
-			System.out.println("Loading data ...");
+		else if(args[0].equals(Constants.ARG_C)) {
+			System.out.println(Constants.LOADING_MESSAGE);
 			String studentNames = fileReadder();
 			char studentNameCharArray[] = studentNames.toCharArray();
 			int studentNumber = 0;
 			for(char c : studentNameCharArray) {
-				if(c == ' ') {
+				if(c == Constants.SPACE) {
 					studentNumber++;
 				}
 			}
-			System.out.println((studentNumber + 1) +" word(s) found");
-			System.out.println("Data Loaded.");
+			System.out.println((studentNumber + 1) + Constants.COUNTED_WORD_MESSAGE);
+			System.out.println(Constants.LOADED_MESSAGE);
 		}
 
-		else if(args[0].charAt(0) == '+'){
-			System.out.println("Loading data ...");
+		else if(args[0].charAt(0) == Constants.ARG_ADD){
+			System.out.println(Constants.LOADING_MESSAGE);
 			String studentNames = fileReadder();
 			String studentToAdd = args[0].substring(1);
 			fileWriter(studentNames, studentToAdd);
-			System.out.println("Data Loaded.");	
+			System.out.println(Constants.LOADED_MESSAGE);
 		}
 
-		else if(args[0].charAt(0) == '?') {
-			System.out.println("Loading data ...");
+		else if(args[0].charAt(0) == Constants.ARG_FIND) {
+			System.out.println(Constants.LOADING_MESSAGE);
 			String studentNames = fileReadder();
-			String studentNameArray[] = studentNames.split(", ");
+			String studentNameArray[] = studentNames.split(Constants.SPLIT);
 			boolean done = false;
 			String studentToFind = args[0].substring(1);
 			for(int idx = 0; idx<studentNameArray.length && !done; idx++) {
 				if(studentNameArray[idx].equals(studentToFind)) {
-					System.out.println("We found it!");
+					System.out.println(Constants.FOUND_MESSAGE);
 					done=true;
 				}
 			}
 			if(!done){
-				System.out.println("Student not found!");
+				System.out.println(Constants.NOT_FOUND_MESSAGE);
 			}
-			System.out.println("Data Loaded.");				
+			System.out.println(Constants.LOADED_MESSAGE);
 		}
 
 		else{
-			System.out.println("Please enter any of these : a | c | r | +word | ?word");
+			System.out.println(Constants.WRONG_INPUT_MESSAGE);
 		}
 	}
 }
